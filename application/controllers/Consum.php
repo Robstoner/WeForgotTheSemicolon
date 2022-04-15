bd0b63d31118377c56c5c5bd406ebf1e9f36cdb9
Ioana Ursoiu <ioanacristina123@gmail.com>
2022-04-16 01:22:43 +0300

    consum add

--- application/controllers/Consum.php
+++ application/controllers/Consum.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Consum extends CI_Controller {
+    
+    public function __construct()
+	{
+		parent::__construct();
+        $this->load->model('everything_model');
+    }
 
     public function index()
     {
@@ -14,5 +20,38 @@ class Consum extends CI_Controller {
         }
         
     }
+
+    public function add()
+    {
+        $data['active'] = 'consum';
+        $this->load->view('add-consum.php', $data);
+    }
+
+    public function add_consum()
+    {
+        $suma = $this->input->post('suma', TRUE);
+
+        $ins = $this->everything_model->consum_add($suma);
+        
+        if ($ins) {
+            $sess = array(
+                'Adaugare_consum_done' => TRUE
+            );
+
+            $this->session->set_flashdata($sess);
+
+            redirect('consum');
+        }
+        else {
+            $sess = array(
+                'Adaugare_consum_failed' => TRUE
+            );
+
+            $this->session->set_flashdata($sess);
+
+            redirect('consum/add');
+        }
+    }
+
 
 }
