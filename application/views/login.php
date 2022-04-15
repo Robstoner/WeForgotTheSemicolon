ced454ef8ca74bae04579c5c43a91958cb80b7df
Schmidt Robert <robstoner19@gmail.com>
2022-04-15 13:07:58 +0300

    Add first page

---
+++ application/views/login.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<html lang="en">
+
+<body>
+    <div>
+        <p style = "text-align: center; color:pink; font-size: 50px">Hello World!</p>
+    </div>
+</body>
